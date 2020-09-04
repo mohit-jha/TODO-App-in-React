14c68a2540706b032ac55b3db888d49f314b8e73
Mohit_jha <mohitjha1511@gmail.com>
2020-09-04 17:41:53 +0530

    Updates

---
+++ precache-manifest.95021aaa901fbba128d96c8b2a45863d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54942fc3ee1da93f4a83731f80008ef9",
+    "url": "/ToDo-App-in-React/index.html"
+  },
+  {
+    "revision": "f7cd5fe5f89f929403ab",
+    "url": "/ToDo-App-in-React/static/css/main.caf35819.chunk.css"
+  },
+  {
+    "revision": "d77cd048d33fe2a0b9a1",
+    "url": "/ToDo-App-in-React/static/js/2.5c387123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-App-in-React/static/js/2.5c387123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7cd5fe5f89f929403ab",
+    "url": "/ToDo-App-in-React/static/js/main.ca6c5edb.chunk.js"
+  },
+  {
+    "revision": "d9080ccd2230c2f4485f",
+    "url": "/ToDo-App-in-React/static/js/runtime-main.8c172644.js"
+  },
+  {
+    "revision": "be3f36434944658d34c48447928aa959",
+    "url": "/ToDo-App-in-React/static/media/690074.be3f3643.jpg"
+  }
+]);
